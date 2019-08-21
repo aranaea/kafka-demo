7aed960627fc6c91dcb410de6f9b348de52c11c4
Peter Cornelius <peter@grabango.com>
2019-08-21 16:05:12 -0700

    Oh look!  Mooore linting changes.

--- producer/src/app.py
+++ producer/src/app.py
@@ -3,7 +3,6 @@ from event_publisher import Publisher
 import logging
 import json
 
-
 app = Flask(__name__)
 dispatcher = Publisher()
 
@@ -12,9 +11,12 @@ logger.setLevel(logging.DEBUG)
 if len(logger.handlers) == 0:
     logger.addHandler(logging.StreamHandler())
 
+
 @app.route("/")
 def index():
-    return "This is the Event generator.  To send an event to the stats processor POST to the /events endpoint."
+    return "This is the Event generator.  To send an event to the stats " + \
+           "processor POST to the /events endpoint."
+
 
 @app.route("/events", methods=['POST'])
 def post_event():
@@ -22,6 +24,7 @@ def post_event():
     logger.debug("request had the following data: {0}".format(message))
     dispatcher.push(message)
     return json.dumps({'status': 'success'}), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
